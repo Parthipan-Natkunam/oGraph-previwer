32ed30b31cc229811ad1dc7e3d00cd8c6916b792
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-01-18 00:19:57 +0530

    add parsing logic for og tags from the current chrome tab

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,3 +1,4 @@
+/* POpup UI Interaction */
 const switchTab = (event) => {
   if (event && event.target) {
     if (event.target.tagName !== "BUTTON") {
@@ -16,3 +17,63 @@ const switchTab = (event) => {
 };
 
 document.getElementById("tabs").addEventListener("click", switchTab);
+
+/*Image Utils*/
+function getImageWidth(imageUrl) {
+  return new Promise(function (resolve) {
+    const img = new Image();
+    img.onload = function () {
+      resolve(this.width);
+    };
+    img.src = imageUrl;
+  });
+}
+
+/*populate UI with data from chrome script execution*/
+function updatePreview(ogData) {
+  if (Object.keys(ogData).length) {
+    const previewContainer = document.getElementById("preview-og-data");
+    const { title, image: imageSrc, description, site_name, url } = ogData;
+    if (previewContainer) {
+      getImageWidth(imageSrc)
+        .then((imgWidth) => {
+          const templateString = `
+          <div
+            style="height:165px;background: url('${imageSrc}') no-repeat top / ${
+            imgWidth >= 362 ? "cover" : "contain"
+          }"
+          ></div>
+          <h2>${title}</h2>
+          <p>
+           ${description}
+          </p>
+          <h4>${site_name ? site_name : url}</h4>
+        `;
+
+          previewContainer.innerHTML = templateString;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }
+}
+
+/*Chrome Tab HTML Parse og tags*/
+const ogParser = `(function(){
+  const ogData = {};
+  const ogTags = document.querySelectorAll('meta[property^="og:"]');
+  ogTags.forEach(tag=>{
+    const key = tag.getAttribute("property").substr(3);
+    const value = tag.getAttribute("content");
+    if(key){
+      ogData[key] = value;
+    }
+  });
+  return ogData;
+})()`;
+
+chrome.tabs.executeScript(null, { code: ogParser }, (result) => {
+  const ogData = result ? result[0] : {};
+  updatePreview(ogData);
+});
