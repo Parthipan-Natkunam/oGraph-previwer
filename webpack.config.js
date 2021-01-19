9e45d252c2a5181985f5dfc4f93852195d6a1c58
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-01-20 03:49:52 +0530

    add minification and bundling logic with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,42 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
   entry: "./src/browser_action/popup.js",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "./manifest.json", to: `${__dirname}/dist/manifest.json` },
+        { from: "./icons", to: `${__dirname}/dist/icons` },
+        {
+          from: "./src/browser_action/browser_action.html",
+          to: `${__dirname}/dist/browser_action.html`,
+        },
+        {
+          from: "./src/browser_action/popup.css",
+          to: `${__dirname}/dist/popup.css`,
+        },
+      ],
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [["optipng", { optimizationLevel: 5 }]],
+      },
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new HtmlMinimizerPlugin(),
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
   output: {
-    path: `${__dirname}/src/browser_action`,
+    path: `${__dirname}/dist`,
     filename: "bundle.js",
   },
   mode: "production",
@@ -10,6 +45,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
+
         use: {
           loader: "babel-loader",
           options: {
@@ -17,7 +53,15 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.png$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
-  devtool: "source-map",
+  // devtool: "source-map",
 };
