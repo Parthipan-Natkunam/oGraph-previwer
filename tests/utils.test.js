58537ce658c13f7ae1095dcc7d402278ad01dbbd
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-02-18 00:13:42 +0530

    add test cases for util method getImageWidth

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -20,3 +20,50 @@ describe("Application Utility Method: getOgParserCodeString", () => {
     expect(getOgParserCodeString()).toEqual(mockIIFECodeString);
   });
 });
+
+describe("Application Utility Method: getImageWidth", () => {
+  test("rejects the promise, when iamgeURL is empty", () => {
+    return getImageWidth().catch((error) => expect(error).toEqual(undefined));
+  });
+  test("rejects the promise, when iamgeURL is null", () => {
+    return getImageWidth(null).catch((error) =>
+      expect(error).toEqual(undefined)
+    );
+  });
+  test("rejects the promise, when iamgeURL is undefined", () => {
+    return getImageWidth(void 0).catch((error) =>
+      expect(error).toEqual(undefined)
+    );
+  });
+  test("rejects the promise, when iamgeURL is an empty string", () => {
+    return getImageWidth("").catch((error) => expect(error).toEqual(undefined));
+  });
+  test("rejects the promise, when iamgeURL is a space only string", () => {
+    return getImageWidth("   ").catch((error) =>
+      expect(error).toEqual(undefined)
+    );
+  });
+  test("rejects the promise, when imag load is errored", () => {
+    global.Image = class {
+      constructor() {
+        setTimeout(() => this.onerror(), 300);
+      }
+    };
+
+    return getImageWidth("nonexistentGibberish.png").catch((error) =>
+      expect(error).toBe(undefined)
+    );
+  });
+  test("resolves the width, when iamgeURL is loaded", () => {
+    global.Image = class {
+      constructor() {
+        this.width = 16;
+        setTimeout(() => this.onload(), 300);
+      }
+    };
+
+    return getImageWidth("../../icons/icon16.png").then((width) =>
+      expect(width).toBe(16)
+    );
+  });
+});
